b8386f27ed066262c094932c2281e2307ae2c803
Ahmad Shah <ahmad1911491@gmail.com>
2025-06-06 19:01:52 +0500

    Update JWT utils

--- src/utils/jwt.util.js
+++ src/utils/jwt.util.js
@@ -2,20 +2,18 @@ const jwt = require("jsonwebtoken");
 const { jwtSecret, jwtExpiresIn } = require("../config/jwt.config");
 const { UnauthorizedError } = require("../errors/error-classes");
 
-const makePayload = (user) => {
-  return {
-    user_id: user.user_id,
-    username: user.username,
-    email_address: user.email_address,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-    deleted_at: user.deleted_at,
-  };
-};
-
 const generateToken = (user) => {
-  const payload = makePayload(user);
-  return jwt.sign(payload, jwtSecret, { expiresIn: jwtExpiresIn });
+  return jwt.sign(
+    {
+      user_id: user.user_id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    },
+    jwtSecret,
+    { expiresIn: jwtExpiresIn }
+  );
 };
 
 const verifyToken = (token) => {
